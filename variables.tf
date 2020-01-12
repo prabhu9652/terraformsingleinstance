dfef0f75c5ac9b891bdf1d4be1fc855d7f35a46d
prabhu9652 <58967128+prabhu9652@users.noreply.github.com>
2020-01-12 12:06:07 +0530

    eddited main terrafile for s3

--- variables.tf
+++ variables.tf
@@ -23,6 +23,10 @@ variable "public_subnet2_name" {}
 variable "public_subnet3_name" {}
 variable "private_subnet_name" {}
 variable Main_Routing_Table {}
+
+variable s3bucketname {}
+
+variable terraformstatefile {}
 variable "azs" {
   description = "Run the EC2 Instances in these Availability Zones"
   type = "list"
@@ -37,5 +41,3 @@ variable "instance_type" {
     prod = "t2.medium"
     }
 }
-
-
